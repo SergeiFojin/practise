3c6d89a86210e31b31251bb0ff150d97dd180f1d
SergeyKim <sergey.kim@fojin.tech>
2022-11-25 11:09:16 +0300

    todo commit

--- html+css/webpage/webpage.js
+++
@@ -1,9 +0,0 @@
-let burger = document.querySelector('.menuBurger');
-if(burger) {
-    let menu = document.querySelector('.headerMenu');
-    let background = document.querySelector('.headerBackground');
-    burger.addEventListener('click', function(e) {
-        menu.classList.toggle('active');
-        background.classList.toggle('active');
-    })
-}
