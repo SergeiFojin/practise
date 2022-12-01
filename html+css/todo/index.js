df1cc1d181dd43e19d4e4df5b927449bedaf1cc5
SergeyKim <sergey.kim@fojin.tech>
2022-12-01 11:49:24 +0300

    review fixes

--- html+css/todo/index.js
+++ html+css/todo/index.js
@@ -2,7 +2,7 @@ const addTaskButton = document.querySelector('.main-form-button');
 const newTaskForm = document.querySelector('.main-form-text');
 const mainList = document.querySelector('.main-list');
 const emptyList = document.createElement('div');
-let todos = JSON.parse(localStorage.getItem('todoTasks'));
+let todos = JSON.parse(localStorage.getItem('todoTasks')) || [];
 
 
 const checkEmptyList = () => {
@@ -14,24 +14,29 @@ const checkEmptyList = () => {
 }
 
 const renderList = () => {
-    for(let i = 0; i < todos.length; i++) {
+
+    todos.map(item => {
+
         const newTask = document.createElement('div');
         newTask.classList.add('main-list-elem');
         newTask.innerHTML = `
                        <div class="main-list-elem-item">
-                           <input class="main-list-elem-item-checkbox" type="checkbox" id="${todos[i].id}">
-                           <label class="main-list-elem-item-checkmark"  for="${todos[i].id}"></label>
+                           <input class="main-list-elem-item-checkbox" type="checkbox" id="${item.id}">
+                           <label class="main-list-elem-item-checkmark"  for="${item.id}"></label>
                        </div>
-                       <input class="main-list-elem-input" type="text" id="${todos[i].id}" value="${todos[i].value}">
-                       <button class="main-list-elem-remove" id="${todos[i].id}"><img class="main-list-elem-remove-img" id="${todos[i].id}" src="source/plus.png"/></button>
+                       <input class="main-list-elem-input" type="text" id="${item.id}" value="${item.value}">
+                       <button class="main-list-elem-remove" id="${item.id}"><img class="main-list-elem-remove-img" id="${item.id}" src="source/plus.png"/></button>
         `;
-        if(todos[i].completed === true) {
+
+        if (item.completed === true) {
             newTask.classList.add('completed');
             newTask.children[0].children[0].setAttribute('checked', true);
             newTask.children[1].setAttribute('disabled', true);
         }
+
         mainList.append(newTask);
-    }
+    })
+
     emptyList.remove();
 }
 
@@ -46,14 +51,17 @@ const addTask = () => {
             todos.push({id: `${Date.now()}`, value: newTaskForm.value, completed: false});
         } else {
             let test = [];
+
             todos.map((item, index, array) => {
                 if (item.completed === true) {
                     test = todos.splice(index, array.length - index);
                 }
             })
+
             todos.push({id: `${Date.now()}`, value: newTaskForm.value, completed: false});
             todos = [...todos, ...test]
         }
+
         newTaskForm.value = '';
     }
 
@@ -62,58 +70,71 @@ const addTask = () => {
 
 const changeTask = (e) => {
     if (e.target.classList.contains('main-list-elem-input')) {
-        for (let i = 0; i < todos.length; i++) {
-            if(todos[i].id === e.target.id) {
-                todos[i].value = e.target.value;
+
+        todos.map(item => {
+            if (item.id === e.target.id) {
+                item.value = e.target.value;
             }
-        }
+        })
+
     }
+
     localStorage.setItem('todoTasks', JSON.stringify(todos));
 }
 
 const completingTask = (e) => {
     const task = e.target.parentNode.parentNode;
+
     if (e.target.classList.contains('main-list-elem-item-checkbox')) {
         task.classList.toggle('completed');
-        for (let i = 0; i < todos.length; i++) {
-            if (todos[i].id === e.target.id) {
-                todos[i].completed = true;
-                let completedTask = todos.splice(i, 1);
-                todos.push(completedTask[0]);
+
+        todos.map((item, index, array) => {
+            if (item.id === e.target.id) {
+                item.completed = true;
+                let completedTask = array.splice(index, 1);
+                array.push(completedTask[0]);
             }
-        }
+        })
+
         task.children[1].setAttribute('disabled', true);
         mainList.children[mainList.children.length-1].after(task);
 
         if (!(task.classList.contains('completed'))) {
-            for (let i = 0; i < todos.length; i++) {
-                if (todos[i].id === e.target.id) {
-                    todos[i].completed = false;
-                    let completedTask = todos.splice(i, 1);
-                    todos.unshift(completedTask[0]);
+
+            todos.map((item, index, array) => {
+                if (item.id === e.target.id) {
+                    item.completed = false;
+                    let completedTask = array.splice(index, 1);
+                    array.unshift(completedTask[0]);
                 }
-            }
+            })
+
             task.children[1].removeAttribute('disabled');
             mainList.children[0].before(task);
         }
+
     }
+
     localStorage.setItem('todoTasks', JSON.stringify(todos));
 }
 
 const removeTask = (e) => {
     const task = e.target.parentNode.parentNode;
+
     if (e.target.classList.contains('main-list-elem-remove-img')) {
-        for (let i = 0; i < todos.length; i++) {
-            if (todos[i].id === e.target.id) {
-                todos.splice(i, 1);
+
+        todos.map((item, index, array) => {
+            if (item.id === e.target.id) {
+                array.splice(index, 1);
             }
-        }
+        })
+
         task.remove();
         checkEmptyList()
     }
+
     localStorage.setItem('todoTasks', JSON.stringify(todos));
 }
-
 
 
 if (addTaskButton) {
@@ -123,7 +144,6 @@ if (addTaskButton) {
         checkEmptyList()
     })
 }
-
 
 if (mainList) {
     mainList.addEventListener('input', changeTask)
